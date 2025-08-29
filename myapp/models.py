2ed7bc8972677bff832bfb615528818fabb6bc2c
Myat ThiHa Tun <thihagongar7@example.com>
2025-08-30 03:41:01 +0630

    error

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+import uuid
 
 # Create your models here.
+
+class BannerModel(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    image = models.ImageField(upload_to='banner/')
+    tagline = RichTextField()
+    title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
 
 def Homepage(request):
-    return render(request,'index.html')
+    banner = BannerModel.objects.all()
+    contex = {
+        'banner':banner
+    }
+    return render(request,'index.html',contex )
